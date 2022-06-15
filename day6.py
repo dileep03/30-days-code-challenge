bdd55442b0c8e676ece7ff40ea456a9ee4b30e73
DILEEP <107190613+dileep03@users.noreply.github.com>
2022-06-15 06:29:03 +0530

    day6 of the 30days code challenge

---
+++ day6.py
@@ -0,0 +1,17 @@
+#30-days-code-challenge- DAY 5(15/6/22)
+#learning python basics
+
+#linear search
+
+def search(arr, x):
+
+	for i in range(len(arr)):
+
+		if arr[i] == x:
+			return i
+
+	return -1
+
+arr = [ 1,3,2,5,7 ]
+result=search(arr,5) 
+print("the required element is at index",result)
