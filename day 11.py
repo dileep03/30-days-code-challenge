02cb946dd1f06aaa4989f13af3216441fc2b4382
DILEEP <107190613+dileep03@users.noreply.github.com>
2022-06-22 21:08:04 +0530

    Update day 11.py

--- day 11.py
+++ day 11.py
@@ -1,3 +1,14 @@
 #30-days-code-challenge- DAY 11(20/6/22)
 #learning python basics
 
+
+#fibonacci series
+a = 0
+b = 1
+n=int(input("Enter the number of terms in the sequence: "))
+print(a,b,end=" ")
+while(n-2):
+    c=a+b
+    a,b = b,c
+    print(c,end=" ")
+    n=n-1
