9fd003ca7d4ae9ec4b726be1360ff7e95d5ad2fb
DILEEP <107190613+dileep03@users.noreply.github.com>
2022-06-22 21:09:59 +0530

    Update day 13.py

---
+++ day 13.py
@@ -0,0 +1,13 @@
+#30-days-code-challenge- DAY 13(22/6/22)
+#learning python basics
+
+#perfect number
+n = int(input("Enter any number: "))
+sum1 = 0
+for i in range(1, n):
+    if(n % i == 0):
+        sum1 = sum1 + i
+if (sum1 == n):
+    print("The number is a Perfect number!")
+else:
+    print("The number is not a Perfect number!")
