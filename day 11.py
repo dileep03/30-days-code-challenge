6f7216ae260ce7486811b6911fb3004aa611eb8a
DILEEP <107190613+dileep03@users.noreply.github.com>
2022-06-22 21:09:31 +0530

    Update day 11.py

--- day 11.py
+++ day 11.py
@@ -2,13 +2,22 @@
 #learning python basics
 
 
-#fibonacci series
-a = 0
-b = 1
-n=int(input("Enter the number of terms in the sequence: "))
-print(a,b,end=" ")
-while(n-2):
-    c=a+b
-    a,b = b,c
-    print(c,end=" ")
-    n=n-1
+# Python Program to find the L.C.M. of two input number
+
+def compute_lcm(x, y):
+   if x > y:
+       greater = x
+   else:
+       greater = y
+
+   while(True):
+       if((greater % x == 0) and (greater % y == 0)):
+           lcm = greater
+           break;
+       greater += 1
+
+   return lcm
+
+num1 = 54
+num2 = 24
+print("The L.C.M. is", compute_lcm(num1, num2))
