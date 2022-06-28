caf3a5650f482df2e4ee02f555813ab5a798bb71
DILEEP <107190613+dileep03@users.noreply.github.com>
2022-06-28 07:18:53 +0530

    day 19

---
+++ day 19.py
@@ -0,0 +1,26 @@
+#30-days-code-challenge- DAY 19(28/6/22)
+#learning python basics
+
+#permutations nPr
+print("Enter the Value of n: ")
+n = int(input())
+print("Enter the Value of r: ")
+r = int(input())
+
+fact = 1
+i = 1
+while i<=n:
+  fact = i*fact
+  i = i+1
+
+numerator = fact        
+sub = n - r         
+fact = 1
+i = 1
+while i<=sub:
+  fact = i*fact
+  i = i+1
+denominator = fact      
+perm = numerator/denominator
+
+print("\nPermutation =", perm)
