03010009b836837d327016096b4d86ac646c3a5d
DILEEP <107190613+dileep03@users.noreply.github.com>
2022-07-03 17:47:49 +0530

    day 24

---
+++ day 24.py
@@ -0,0 +1,9 @@
+#30-days-code-challenge- DAY 24(3/7/22)
+#learning python basics
+
+# calendar
+import calendar  
+yy = int(input("Enter year: "))
+mm = int(input("Enter month: "))
+
+print(calendar.month(yy, mm))
